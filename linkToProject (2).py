7f11ea84ec1496af1e31392398d2d7db12a4c270
aradhan123 <151199876+aradhan123@users.noreply.github.com>
2023-12-15 16:14:02 -0500

    Add files via upload

---
+++ linkToProject (2).py
@@ -0,0 +1,20 @@
+#Click the link below to access our project. Veiw the project's readme.txt for more information:
+print("Click here:")
+print("https://drive.google.com/drive/folders/1OVREUWQNtJXKP53tuc1vyj_Y_MGk07NH?usp=drive_link")#link is inside of the print() statement
+
+'''Team:
+░██████╗░░█████╗░██╗███╗░░██╗░██████╗░  ██████╗░░█████╗░███╗░░██╗░█████╗░███╗░░██╗░█████╗░░██████╗
+██╔════╝░██╔══██╗██║████╗░██║██╔════╝░  ██╔══██╗██╔══██╗████╗░██║██╔══██╗████╗░██║██╔══██╗██╔════╝
+██║░░██╗░██║░░██║██║██╔██╗██║██║░░██╗░  ██████╦╝███████║██╔██╗██║███████║██╔██╗██║███████║╚█████╗░
+██║░░╚██╗██║░░██║██║██║╚████║██║░░╚██╗  ██╔══██╗██╔══██║██║╚████║██╔══██║██║╚████║██╔══██║░╚═══██╗
+╚██████╔╝╚█████╔╝██║██║░╚███║╚██████╔╝  ██████╦╝██║░░██║██║░╚███║██║░░██║██║░╚███║██║░░██║██████╔╝
+░╚═════╝░░╚════╝░╚═╝╚═╝░░╚══╝░╚═════╝░  ╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═════╝░
+
+
+Names: Stephen Radice, Anthony Luchette, Aarya Radhan
+
+Pledges: 
+  I pledge my Honor I have abided by the Stevens Honor System - SR
+  I pledge my Honor I have abided by the Stevens Honor System - AL
+  I pledge my Honor I have abided by the Stevens Honor System - AR
+'''
